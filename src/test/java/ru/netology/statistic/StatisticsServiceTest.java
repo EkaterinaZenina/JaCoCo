b793272ddd277f78c0e950aee65b5639b95feb80
Ekaterina.Zenina <zeninaekaterin@gmail.com>
2021-11-02 09:29:02 +0300

    fix

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,9 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxFromMin() {
         StatisticsService service = new StatisticsService();
 
+        
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
